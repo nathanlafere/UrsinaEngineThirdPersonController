bbd60cf73a1d19020e0d9074f5a158557821249d
Nathan Lafere <nathanlafere@gmail.com>
2023-05-22 23:04:42 -0300

    triangulation issues test

--- UrsinaEngineThirdPerson/Main.py
+++ UrsinaEngineThirdPerson/Main.py
@@ -15,12 +15,9 @@ def input(key):
 data.ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4))
 Sky()
 
+poring = load_model("assets/Poring.gltf")
 enemy_01 = enemy.Enemy(actor_model="assets/Poring.gltf", scale=2, position=(25,500,10))
-enemy_02 = enemy.Enemy(actor_model="assets/Poring.gltf", scale=2, position=(20,500,10))
-enemy_03 = enemy.Enemy(actor_model="assets/Poring.gltf", scale=2, position=(15,500,10))
-
-for c in range(15):
-    enemy.Enemy(actor_model="assets/Poring.gltf", scale=2, position=(5*c+1,500,10))
+enemy_01.collider.visible = True
 
 ex_portal = portal.Portal(position=(10,0,10),exit_position=(20,0,-20))
 player = third_person_controller.ThirdPersonController()
