083d040dc1dbdb1f0f490a2ca7e32a9541a29f01
Nathan Lafere <nathanlafere@gmail.com>
2023-05-30 20:16:11 -0300

    ground fixes

--- UrsinaEngineThirdPerson/Main.py
+++ UrsinaEngineThirdPerson/Main.py
@@ -10,27 +10,31 @@ import itertools
 app = Ursina()
 
 
-    
 def input(key):
     if key == 'escape':
         quit()
+    if key == 'c':
+        data.ground.position += Vec3(1,0,0)
 
 #data.ground = Entity(model='plane', scale=100, texture='grass', collider='box')
-noise = PerlinNoise(octaves=1,seed=1)
-terrain_width = 32
+noise = PerlinNoise(octaves=3,seed=1)
+terrain_width = 30
+
 
 for z, x in itertools.product(range(terrain_width), range(terrain_width)):
     y = noise([x/terrain_width, z/terrain_width])*5
-    block = Entity(model='plane', position=(x-terrain_width/2,y,z-terrain_width/2), scale=(1,0.1,1))
-    block.parent = data.ground
-    block.rotation_x = (noise([x/terrain_width, (z-1)/terrain_width])*5 -block.y)*56.4
-    block.rotation_z = (noise([(x-1)/terrain_width, z/terrain_width])*5 -block.y)*56.4
-data.ground.combine()
-data.ground.collider = 'mesh'
-data.ground.texture = 'white_cube'
+    data.ground.vertices.extend([
+        (x-terrain_width/2 +0.5, y+ (noise([(x+1)/terrain_width, (z+1)/terrain_width])*5 -y), z-terrain_width/2 +0.5),
+        (x-terrain_width/2 -0.5, y+ (noise([(x)/terrain_width, (z+1)/terrain_width])*5 -y), z-terrain_width/2 +0.5),
+        (x-terrain_width/2 -0.5, y, z-terrain_width/2 -0.5),
+        (x-terrain_width/2 +0.5, y+ (noise([(x+1)/terrain_width, (z)/terrain_width])*5 -y), z-terrain_width/2 -0.5),
+        (x-terrain_width/2 +0.5, y+ (noise([(x+1)/terrain_width, (z+1)/terrain_width])*5 -y), z-terrain_width/2 +0.5),
+        (x-terrain_width/2 -0.5, y, z-terrain_width/2 -0.5)])
+
+data.ground = Entity(model=Mesh(vertices=data.ground.vertices, uvs=data.ground.vertices),texture='grass')
+data.ground.collider = MeshCollider(data.ground)
 
 Sky()
-
 enemy_01 = enemies.Enemy(actor_model="assets/Poring.gltf", scale=2, position=(25,0,10))
 enemy_01 = enemies.Enemy(actor_model="assets/Poring.gltf", scale=3, position=(25,0,15))
 enemy_01 = enemies.Enemy(actor_model="assets/Poring.gltf", scale=4, position=(25,0,20))
@@ -38,6 +42,5 @@ ex_portal = structs.Portal(position=(10,0,10),exit_position=(20,0,-20), rotation
 
 
 player = third_person_controller.ThirdPersonController()
-structs.Bridge((40,0,0))
 
 app.run()
